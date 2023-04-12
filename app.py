780a7e6d712e3ee0b0846e23d8f028337087d735
Imtiyaz <vayporwayv@gmail.com>
2023-04-12 19:58:25 +0100

    test

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
-import os
 
 import openai
-from flask import Flask, redirect, render_template, request, url_for
 
-app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
 
+def test_animal_response(animal: str):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=generate_prompt(animal),
+        temperature=0.6,
+    )
+    return response.choices[0].text
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
 
 
 def generate_prompt(animal):
@@ -33,3 +24,6 @@ Animal: {}
 Names:""".format(
         animal.capitalize()
     )
+
+if __name__ == "__main__":
+    print(test_animal_response("bird"))
